9aa1397200b20f39e6c0629f3988a9cb8771b7b5
Kevin Dunn <KDunn@martincurrie.com>
2017-08-04 09:59:05 +0100

    next

--- test.ps1
+++ test.ps1
@@ -1,4 +1,19 @@
 #Azure login
 Login-AzureRmAccount
+# Create a resource group.
+ New-AzureRmResourceGroup -Name 'New Vnet with subnets' -Location 'westeurope'
+ # Create a virtual network
+ $Vnet=New-AzureRmVirtualNetwork `
+  -ResourceGroupName MyResourceGroup `
+  -Location westeurope `
+  -Name MyVnet `
+  -AddressPrefix 192.168.0.0/16 `
+  -Subnet $Subnet1,$Subnet2
+# Create two subnets.
+$Subnet1 = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name Public `
+  -AddressPrefix 192.168.0.0/16
+$Subnet2 = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name Private `
+  -AddressPrefix 192.168.0.0/16
 
-     o;;       
