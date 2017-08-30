6de26bc50a8ac05731cba7b4219407d12c73d717
Kevin Dunn <KDunn@martincurrie.com>
2017-08-30 10:26:49 +0100

    up

---
+++ test 2.ps1
@@ -0,0 +1,5 @@
+write-host "hello world"
+
+$m = get-credential
+
+write-host $m
--- test.ps1
+++ test.ps1
@@ -1,4 +1,6 @@
 #Azure login
+#Create a resource group
+
 Login-AzureRmAccount
 # Create a resource group.
  New-AzureRmResourceGroup -Name 'New Vnet with subnets' -Location 'westeurope'
@@ -17,3 +19,8 @@ $Subnet2 = New-AzureRmVirtualNetworkSubnetConfig `
   -Name Private `
   -AddressPrefix 192.168.0.0/16
 
+  $storage = New-azurestorage
+
+
+
+  
